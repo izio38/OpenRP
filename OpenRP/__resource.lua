109531ac3f06458a6bb2efb175e64fd2820176ec
LMA.Doneur <contact@leavemealone.fr>
2017-08-10 16:53:39 +0200

    adding Folder

---
+++ OpenRP/__resource.lua
@@ -0,0 +1,13 @@
+resource_type 'gametype' { name = 'default fivem server' }
+
+server_scripts {
+	"server/main.lua"
+}
+
+client_scripts {
+	"client/main.lua"
+}
+
+files {
+	--'meta/mapzoomdata.meta'
+}
