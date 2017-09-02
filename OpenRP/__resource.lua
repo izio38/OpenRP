41aedcdac0981f4afebc5886f75a2b143271947d
izio38 <romainbillot3009@gmail.com>
2017-09-02 16:08:38 +0200

    Update ALL

--- OpenRP/__resource.lua
+++ OpenRP/__resource.lua
@@ -1,16 +1,35 @@
-resource_type 'gametype' { name = 'default fivem server' }
+resource_type 'gametype' { name = 'OpenRp ' .. GetOPVersion() }
 
 server_scripts {
 	"server/main.lua",
+	"config/main.lua",
+	"class/user.lua"
+
+	-- LIBRARIES
 	"@mysql-async/lib/MySQL.lua",
-	"config.lua"
+	"lib/i18n.lua",
+
+		-- LOCALES
+	'config/locales/en-GB.lua',
+	'config/locales/fr-FR.lua'
 }
 
 client_scripts {
 	"client/main.lua",
-	"config.lua"
+	"config/main.lua",
+
+	-- LIBRARIES
+	"lib/i18n.lua",
+
+	-- LOCALES
+	'config/locales/en-GB.lua',
+	'config/locales/fr-FR.lua',
 }
 
 files {
 	--'meta/mapzoomdata.meta'
 }
+
+function GetOPVersion()
+	return "0.1b" -- change to PerformHttpRequest?
+end
---
+++ OpenRP/config/main.lua
@@ -0,0 +1,12 @@
+OpenRp.Config = {
+	["WhiteList"] = 1,
+	["Steam"] = 1,
+	["Lang"] = "fr-FR" -- currently available: en-GB / fr-FR
+}
+
+OpenRp.firstUserDatas = {
+    ["money"] = 500,
+    ["bankMoney"] = 500,
+    ["job"] = "Chômeur",
+    ["whiteListed"] = 0
+}
