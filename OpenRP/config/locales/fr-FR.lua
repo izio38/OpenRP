e882c11dee0ed2461e5879a396aae6fe3f6fd45a
NicolasStr_ <nicolas.streng@gmail.com>
2017-09-02 13:21:28 +0200

    Added i18n integration, en-GB/fr-FR locales added

---
+++ OpenRP/config/locales/fr-FR.lua
@@ -0,0 +1,5 @@
+i18n.importData("fr-FR", {
+  init="OpenRP initialisé",
+  SteamMustBeLaunched="Vous devez être connecté via Steam pour pouvoir vous connecter sur ce serveur.",
+  NeedToBeWhitelisted="Vous devez être white-listé pour pouvoir jouer sur ce serveur."
+})
---
+++ OpenRP/config/main.lua
@@ -0,0 +1,5 @@
+OpenRp.Config = {
+	["WhiteList"] = 1,
+	["Steam"] = 1,
+	["Lang"] = "fr-FR" -- currently available: en-GB / fr-FR
+}
